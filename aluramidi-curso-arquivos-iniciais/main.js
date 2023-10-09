a744ac095b65257c07c21f5635699aaea3eaedb5
slvmaria <130380578+slvmaria@users.noreply.github.com>
2023-10-09 07:58:02 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -9,8 +9,16 @@ let contador = 0;
 //enquanto
 while (contador < listaDeTeclas.length)  {
   
-    listaDeTeclas[contador].onclick = function () {
-          tocaSom('#som_tecla_pom')
+ const tecla = listaDeTeclas[contador];
+ const instrumento = tecla.classList[1];
+ 
+ 
+ const idAudio = `som_${instrumento}`
+
+ console.log(idAudio);
+ 
+   tecla.onclick = function () {
+          tocaSom(idAudio)
     }
       
     contador = contador + 1
