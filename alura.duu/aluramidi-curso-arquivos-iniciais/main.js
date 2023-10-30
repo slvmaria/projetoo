751e17e77452a4e68effae0ecaf0d68972542cc3
slvmaria <130380578+slvmaria@users.noreply.github.com>
2023-10-30 11:07:07 -0300

    Add files via upload

---
+++ alura.duu/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,28 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const  listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length)  { 
+
+    const tecla = listaDeTeclas[contador]
+
+    const instrumento = listaDeTeclas [contador].classList[1];
+
+    console.log(instrumento)
+     
+    idAudio = `#som_${instrumento}`;
+  
+    listaDeTeclas [contador].onclick = function () {
+       tocaSom('#som_tecla_pom');
+    }
+      
+    contador = contador + 1
+    
+    console.log(contador);
+
+}
